f544c413721b54891788079615c4941db6b35164
Salman12345a <syedsalmn1234786@gmail.com>
2025-09-12 00:36:16 +0530

    implemented a production graded app

--- src/components/navigation/CustomDrawerContent.tsx
+++ src/components/navigation/CustomDrawerContent.tsx
@@ -173,7 +173,7 @@ const CustomDrawerContent = (props: DrawerContentComponentProps) => {
           </View>
           <Text style={styles.logoutText}>Logout</Text>
         </TouchableOpacity>
-        <Text style={styles.versionText}>Version 2.8.9</Text>
+        <Text style={styles.versionText}>Version 2.9.0</Text>
       </View>
     </SafeAreaView>
   );
